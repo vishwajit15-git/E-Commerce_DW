3e55166f84c1620d2738885301e43aa06e0ffe5b
vishwajit15-git <hodgarvishwajit15@gmail.com>
2026-07-19 12:02:10 +0530

    feat: add stores, coupons, orders, order_items, and reviews tables to OLTP schema

--- schema_oltp.sql
+++ schema_oltp.sql
@@ -64,4 +64,82 @@ CREATE TABLE products(
 
 );
 
+-- tables 5: stores
+CREATE TABLE stores(
+    -- details
+    store_id INT PRIMARY KEY AUTO_INCREMENT,
+    store_name VARCHAR(100) NOT NULL,
+
+    -- OneStop has Physical stores and an online
+    store_type ENUM('Physical', 'Online') NOT NULL,
+
+
+    -- address
+    city VARCHAR(50),
+    state VARCHAR(50),
+    pincode CHAR(6)
+    
+);
+
+-- table 6: coupons
+CREATE TABLE coupons(
+    coupon_id INT PRIMARY KEY AUTO_INCREMENT,
+    coupon_code VARCHAR(20) NOT NULL UNIQUE,
+    discount_percentage DECIMAL(5,2),
+    coupon_expiry DATETIME,
+    is_active BOOLEAN DEFAULT TRUE
+);
+
+
+-- table 7: orders  [most important table in our e-commerce]
+
+CREATE TABLE orders(
+
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    status ENUM('Pending','Shipped','Delivered','Cancelled') DEFAULT 'Pending',
+    order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    total_amount DECIMAL(12,2) NOT NULL,
+    customer_id INT,
+    store_id INT,
+    coupon_id INT,
+
+
+    -- FOREIGN KEYS
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (store_id) REFERENCES stores(store_id),
+    FOREIGN KEY (coupon_id) REFERENCES coupons(coupon_id)
+
+);
+
+-- table 8 :order_items
+
+CREATE TABLE order_items(
+    order_item_id INT PRIMARY KEY AUTO_INCREMENT,
+    quantity INT NOT NULL,
+    unit_price DECIMAL(12,2) NOT NULL,
+
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+
+    -- FOREIGN KEY
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+
+-- table 9: reviews
+
+CREATE TABLE reviews(
+    review_id INT PRIMARY KEY AUTO_INCREMENT,
+    rating INT NOT NULL CHECK(rating BETWEEN 1 AND 5) ,
+    comment TEXT,
+    review_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    customer_id INT,
+    product_id INT,
+
+    -- FOREIGN KEY
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
 SHOW TABLES;
